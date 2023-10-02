3de2db1da9010eb0cbbed80072ff163110757e1d
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-10-02 16:00:10 +0200

    MjJ style

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 let socket = io("http://localhost:3000", { autoConnect: true });
 
 const app = createApp(App);
-app.config.globalProperties.$socket = socket; // Provide socket to all components
+app.config.globalProperties.$socket = socket; // Fournit un socket à tous les composants
 
 socket.on("connect", function () {
   console.log("connected");
